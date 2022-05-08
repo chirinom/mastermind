3e59255a3891874c58bc020a547bb0826e19f337
Miguel <mchirinot@gmail.com>
2022-05-08 18:31:01 +0200

    cleaning

--- src/components/ColorsBar.vue
+++ src/components/ColorsBar.vue
@@ -1,31 +1,69 @@
 <template>
-  <div class="colors-bar">
-    <ColorsBarList :options="availableColors"/>
+  <div class="bar-container">
+    <div v-if="availableColors.length" class="current-color">
+      <span class="label">Current Color</span>
+      <Button :color="currentColor" :disabled="true"/>
+    </div>
+    <Button
+      v-for="(color, index) in availableColors" :key="index"
+      @click="setCurrentColor(color)"
+      :color="color"
+      :disabled="false"
+    />
   </div>
 </template>
 
 <script>
-import ColorsBarList from './ColorsBarList.vue'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
+import Button from './Button.vue'
 
 export default {
   name: 'ColorsBar',
   components: {
-    ColorsBarList
+    Button
   },
   computed: {
-    ...mapGetters(['availableColors'])
+    ...mapGetters(['availableColors', 'currentColor'])
+  },
+  methods: {
+    ...mapMutations(['setCurrentColor'])
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.colors-bar {
-  padding: 30px 0;
+.bar-container {
+  max-width: 77px;
+  padding: 30px 15px 0 0;
+
+  & .current-color {
+    border-style: solid;
+    border-width: 1px;
+    border-color: lightgray;
+    border-radius: 4px;
+    padding: 2px;
+    cursor: not-allowed;
+
+    & button {
+      cursor: not-allowed;
+    }
+    & .label {
+      font-size: 12px;
+      font-weight: bold;
+    }
+  }
 }
 @media screen and (max-width: 580px) {
-  .colors-bar {
+  .bar-container {
     padding: 0;
+    max-width: none;
+    width: 100%;
+    display: flex;
+
+    & .current-color {
+      margin: 0 0 12px;
+    }
   }
+
 }
 </style>
